d849cc2f9c88db5841bb7ecaaac6e9974272ae3a
Stephen Lynch <stepheng.lynch@gmail.com>
2023-12-02 00:27:03 +0000

    Answered part 1

--- day1-part1/src/main.rs
+++ day1-part1/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input/input.txt").unwrap();
+    let contents = contents.trim();
+
+    let mut sum_of_values = 0;
+
+    for line in contents.lines() {
+        let mut nums = vec![];
+        for c in line.chars() {
+            if c.is_numeric() {
+                nums.push(c);
+            }
+        }
+        let combination = vec![nums[0], *nums.last().unwrap()];
+        let value: u32 = combination.into_iter().collect::<String>().parse().unwrap();
+        sum_of_values += value;
+    }
+
+    println!("Answer = {}", sum_of_values);
 }

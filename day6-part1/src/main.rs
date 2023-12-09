a040a8c67565491fcf3c8bc4c328f9576f2b3d82
Stephen Lynch <stepheng.lynch@gmail.com>
2023-12-09 19:25:16 +0000

    day 6 - Rust implementation of hand calculation

--- day6-part1/src/main.rs
+++ day6-part1/src/main.rs
@@ -1,3 +1,53 @@
+use std::fs;
+use std::str;
+use regex::Regex;
+use lazy_static::lazy_static;
+
+fn parse_input(input: &str) -> Vec<(usize, usize)> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(?x)
+        Time:\s+(\d*)\s*(\d*)\s*(\d*)\s*(\d*)\n
+        Distance:\s+(\d*)\s*(\d*)\s*(\d*)\s*(\d*)").unwrap();
+    }
+
+    let cap = RE.captures(input).unwrap();
+    
+    let mut times = vec![];
+    for i in 1..5 {
+        if let Some(time_match) = cap.get(i) {
+            times.push(time_match.as_str().parse().unwrap())
+        }
+    }
+
+    let mut dists = vec![];
+    for i in 1..5 {
+        if let Some(dist_match) = cap.get(i + 4) {
+            dists.push(dist_match.as_str().parse().unwrap())
+        }
+    }
+
+    times.into_iter().zip(dists).collect()
+}
+
+fn calc_lower_upper(dist: usize, time: usize) -> (usize, usize) {
+    let dist = dist as f64;
+    let time = time as f64;
+    let lower = time/2. - f64::sqrt(time.powi(2) / 4. - dist);
+    let upper = time/2. + f64::sqrt(time.powi(2) / 4. - dist);
+    return (lower.ceil() as usize, upper.floor() as usize)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input/input.txt").unwrap();
+    let contents = contents.trim();
+
+    let races = parse_input(contents);
+
+    let mut answer = 1;
+    for (time, dist) in races {
+        let (lower, upper) = calc_lower_upper(dist, time);
+        answer *= upper - lower + 1;
+    }
+
+    println!("answer = {}", answer);
 }
--- day6-part2/src/main.rs
+++ day6-part2/src/main.rs
@@ -1,3 +1,50 @@
+use std::fs;
+use std::str;
+use regex::Regex;
+use lazy_static::lazy_static;
+
+fn parse_input(input: &str) -> (usize, usize) {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(?x)
+        Time:\s+(\d*)\s*(\d*)\s*(\d*)\s*(\d*)\n
+        Distance:\s+(\d*)\s*(\d*)\s*(\d*)\s*(\d*)").unwrap();
+    }
+
+    let cap = RE.captures(input).unwrap();
+    
+    let mut time_s = String::new();
+    for i in 1..5 {
+        if let Some(time_match) = cap.get(i) {
+            time_s.push_str(time_match.as_str())
+        }
+    }
+
+    let mut dist_s = String::new();
+    for i in 1..5 {
+        if let Some(dist_match) = cap.get(i + 4) {
+            dist_s.push_str(dist_match.as_str())
+        }
+    }
+
+    (time_s.parse().unwrap(), dist_s.parse().unwrap())
+}
+
+fn calc_lower_upper(dist: usize, time: usize) -> (usize, usize) {
+    let dist = dist as f64;
+    let time = time as f64;
+    let lower = time/2. - f64::sqrt(time.powi(2) / 4. - dist);
+    let upper = time/2. + f64::sqrt(time.powi(2) / 4. - dist);
+    return (lower.ceil() as usize, upper.floor() as usize)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input/input.txt").unwrap();
+    let contents = contents.trim();
+
+    let (time, dist) = parse_input(contents);
+
+    let (lower, upper) = calc_lower_upper(dist, time);
+    let answer = upper - lower + 1;
+
+    println!("answer = {}", answer);
 }

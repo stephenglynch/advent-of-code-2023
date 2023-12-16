200c1ca6e4a70e0b4be465759fc7a5664c0e8a86
Stephen Lynch <stepheng.lynch@gmail.com>
2023-12-16 22:06:08 +0000

    day 15 part 1

--- day15-part1/src/main.rs
+++ day15-part1/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+use std::str;
+
+fn calc_hash(input: &str) -> u8 {
+    input.bytes().fold(0, |acc, c| {
+        acc.wrapping_add(c).wrapping_mul(17)
+    })
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input/input.txt").unwrap();
+    let contents = contents.trim();
+    let answer = contents.split(',').fold(0, |acc, s| {
+            acc + calc_hash(&s) as u64
+    });
+
+    println!("answer = {}", answer);
 }
